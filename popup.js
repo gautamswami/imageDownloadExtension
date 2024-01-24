3dfeae44df57b9a865b3d7d43cc9a7028815445b
gautamswami <gautams5974@gmail.com>
2024-01-25 00:49:45 +0530

    wrappingUp

--- popup.js
+++ popup.js
@@ -11,6 +11,83 @@ document.getElementById("imagePick").addEventListener("click", function () {
     );
   });
 });
+
+document.getElementById("mediaPick").addEventListener("click", function () {
+  handleButtonSelection("mediaPick");
+  const container = document.getElementById("imageContainer");
+  container.innerHTML = ""; // Remove existing content
+
+  const message = document.createElement("p");
+  message.className = "boldText";
+  message.textContent = `COMING SOON ! JOIN WAITING LIST`; // Added line break
+  message.style.textDecoration = "underline";
+  message.style.textAlign = "center";
+  container.appendChild(message);
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("formContainer");
+  const emailInput = document.createElement("input");
+  emailInput.classList.add("emailInput");
+  emailInput.type = "email";
+  emailInput.placeholder = "Enter your email";
+  
+
+  const signupButton = document.createElement("button");
+  signupButton.textContent = "Sign Up";
+  signupButton.classList.add("signupButton");
+  signupButton.addEventListener("click", function () {
+    const email = emailInput.value;
+    // Perform signup logic here
+    emailInput.remove();
+    signupButton.remove();
+
+    // Show notification text
+    const notifyText = document.createElement("p");
+    notifyText.classList.add("notifyText");
+    notifyText.textContent = "We will notify you very soon!";
+    container.appendChild(notifyText);
+    console.log("Signed up with email:", email);
+  });
+  formContainer.appendChild(emailInput);
+  formContainer.appendChild(signupButton);
+  container.appendChild(formContainer);
+  
+});
+
+document.getElementById("fontPick").addEventListener("click", function () {
+  console.log("fontPick");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {
+        code: `
+        console.log("fontPick2");
+        (function() {
+          const allElements = document.querySelectorAll('*');
+          const usedFonts = new Set();
+
+          allElements.forEach(element => {
+            const computedStyle = window.getComputedStyle(element);
+            const fontFamily = computedStyle.getPropertyValue('font-family');
+            if (fontFamily) {
+              fontFamily.split(',').forEach(font => usedFonts.add(font.trim()));
+            }
+          });
+
+          return Array.from(usedFonts);
+        })();
+        `,
+      },
+      function (fonts) {
+        displayFonts(fonts);
+      }
+    );
+  });
+});
+
+document.getElementById("colorPick").addEventListener("click", function () {
+  findColorsAndDisplay();
+});
+
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   chrome.tabs.executeScript(
     tabs[0].id,
@@ -62,37 +139,6 @@ function displayImages(imageUrls) {
   });
 }
 
-document.getElementById("fontPick").addEventListener("click", function () {
-  console.log("fontPick");
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {
-        code: `
-        console.log("fontPick2");
-        (function() {
-          const allElements = document.querySelectorAll('*');
-          const usedFonts = new Set();
-
-          allElements.forEach(element => {
-            const computedStyle = window.getComputedStyle(element);
-            const fontFamily = computedStyle.getPropertyValue('font-family');
-            if (fontFamily) {
-              fontFamily.split(',').forEach(font => usedFonts.add(font.trim()));
-            }
-          });
-
-          return Array.from(usedFonts);
-        })();
-        `,
-      },
-      function (fonts) {
-        displayFonts(fonts);
-      }
-    );
-  });
-});
-
 function displayFonts(fonts) {
   handleButtonSelection("fontPick");
   console.log(fonts);
@@ -121,10 +167,6 @@ function displayFonts(fonts) {
     });
   });
 }
-
-document.getElementById("colorPick").addEventListener("click", function () {
-  findColorsAndDisplay();
-});
 
 function findColorsAndDisplay() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -182,80 +224,71 @@ function displayColors(colors) {
 
   uniqueColors[0].forEach((color) => {
     const div = document.createElement("div");
-    div.className = "contentDiv";
+    div.className = "contentDiv paleteBG";
     const colorBox = document.createElement("div");
     colorBox.style.backgroundColor = color;
     colorBox.className = "colorBox";
-    colorBox.style.width = "115px";
+    colorBox.style.width = "80px";
     colorBox.style.height = "40px";
     colorBox.style.marginBottom = "20px";
     colorBox.style.position = "relative";
+    colorBox.style.border = "1px dashed #6c594c";
 
     const colorText = document.createElement("p");
+    const hexColor = rgbToHex(color);
     colorText.className = "colorText";
-    colorText.textContent = color;
+    colorText.textContent = hexColor;
 
     colorBox.appendChild(colorText); // Add colorText to colorBox first
     div.appendChild(colorBox); // Add colorBox to div
     container.appendChild(div);
     div.addEventListener("click", function () {
-      copyTextFunction(color);
+      copyTextFunction(hexColor);
     });
   });
 }
 
 function handleButtonSelection(buttonId) {
   const buttons = document.getElementsByClassName("headerButton");
+  
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].classList.remove("imageButton");
   }
   document.getElementById(buttonId).classList.add("imageButton");
-}
 
-function copyTextFunction(copyText) {
-  console.log(copyText);
-  navigator.clipboard.writeText(copyText);
-
-  // Alert the copied text
-}
-
-function handleButtonSelection(buttonId) {
-  const buttons = document.getElementsByClassName("headerButton");
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("imageButton");
+  if (buttonId === "colorPick") {
+    document.getElementById("infoText").style.display = "block";
+  } else {
+    document.getElementById("infoText").style.display = "none";
   }
-  document.getElementById(buttonId).classList.add("imageButton");
+  if (buttonId === "fontPick") {
+    document.getElementById("fontText").style.display = "block";
+  } else {
+    document.getElementById("fontText").style.display = "none";
+  }
 }
 function copyTextFunction(copyText) {
   console.log(copyText);
   navigator.clipboard.writeText(copyText);
-
-  // Alert the copied text
+  const copiedText = document.querySelector("#copiedText");
+  copiedText.style.opacity = 1;
+  setTimeout(function () {
+    copiedText.style.opacity = 0;
+  }, 1000);
 }
+function rgbToHex(rgb) {
+  // Check if the color is already in Hex format
+  if (/^#[0-9A-F]{6}$/i.test(rgb)) {
+    return rgb;
+  }
 
-document.getElementById("mediaPick").addEventListener("click", function () {
-  handleButtonSelection("mediaPick");
-  const container = document.getElementById("imageContainer");
-  container.innerHTML = ""; // Remove existing content
+  // Extract individual color components
+  const [r, g, b] = rgb.match(/\d+/g);
 
-  const message = document.createElement("p");
-  message.className = "boldText";
-  message.textContent = `COMING SOON JOIN WAITING LIST`; // Added line break
-  message.style.textDecoration="underline"
-  message.style.textAlign="center"
-  container.appendChild(message);
+  // Convert each component to Hex and concatenate
+  const hexColor = `#${Number(r).toString(16).padStart(2, "0")}${Number(g)
+    .toString(16)
+    .padStart(2, "0")}${Number(b).toString(16).padStart(2, "0")}`;
 
-  const emailInput = document.createElement("input");
-  emailInput.type = "email";
-  emailInput.placeholder = "Enter your email";
-  container.appendChild(emailInput);
-
-  const signupButton = document.createElement("button");
-  signupButton.textContent = "Sign Up";
-  signupButton.addEventListener("click", function () {
-    const email = emailInput.value;
-    // Perform signup logic here
-    console.log("Signed up with email:", email);
-  });
-  container.appendChild(signupButton);
-});
+  return hexColor;
+}
